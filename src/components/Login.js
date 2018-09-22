28297672942c5c4e2ac48889962ad36c0a619fcb
Juan Sebastian Peña <jspenaq@unal.edu.co>
2018-09-22 00:05:54 -0500

    Se creo la carpeta src y un esqueleto muy basico del Login

---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Segundo commit. Yo Soy Muy Inteligente.</Text>
+        <Text>Todos los derechos reservados.</Text>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});

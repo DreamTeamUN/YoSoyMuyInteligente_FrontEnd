fcead4cfa7dc62f90d2122f515e205c275c3939f
Juan Sebastian Peña <jspenaq@unal.edu.co>
2018-09-24 17:33:52 -0500

    Modificado archivo App.js para ver el Login al inicio

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/components/Login'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Primer commit. Yo Soy Muy Inteligente.</Text>
-        <Text>Todos los derechos reservados.</Text>
-      </View>
+      <Login />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

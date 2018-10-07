eafc94e0d023c323f98ec59d1de4992a5759c06f
Alejandro Sebastian Alejo Patarroyo <asalejop@unal.edu.co>
2018-10-07 06:45:11 -0500

    Native Base agregado
    Se agregó nativeBase al proyecto, se implementó en la pantalla de inicio, falta darle más estilo a esa pantalla, también hay que implementar las demás vistas con NativeBase.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,44 +1,88 @@
 import React, { Component } from 'react';
 import { Alert, Dimensions, StyleSheet, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, View } from 'react-native';
-import {Text, Button} from 'react-native-paper';
 import styles from '../styles';
+import Expo from "expo";
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Grid, Col, Row, Left, Right, Body, Icon, Text } from 'native-base';
 
 export default class Main extends Component {
   // static navigationOptions = {
   //   title: 'Login',
   // };
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-  render() {
-    return (
-      <View >
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
 
-        <View style={styles.textContainer}>
-          <Text style={{fontSize:52, textAlign: 'center', textAlignVertical: 'center'}}>
-            Yo Soy Muy Inteligente UN</Text>
-        </View>
+ render() {
+   if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
+   return (
+    <Container>
+      <Content>
+        <Grid>
+          <Row>
+            <Text style={{fontSize:52, textAlign: 'center', textAlignVertical: 'center', margin:10}}>
+                Yo Soy Muy Inteligente UN</Text>
+          </Row>
 
-        <View style={styles.buttonsContainer}>
+          <Row>
+            <Button iconLeft style = {{margin:10}} onPress={() => this.props.navigation.navigate('MainSignUp')}>
+                <Icon name="person-add" />
 
-            <View style={styles.button}>
-              <Button
-                mode="contained"
-                icon="person-add"
-                onPress={() => this.props.navigation.navigate('MainSignUp')}>
-                Registro</Button>
-            </View>
+                <Text>Registro</Text>
+            </Button>
 
+            <Button iconLeft style = {{margin:10}} onPress={() => this.props.navigation.navigate('MainSignUp')}>
+                <Icon name="person" />
+                <Text>Iniciar Sesión</Text>
+            </Button>
+          </Row>
+        </Grid>
+      </Content>
+    </Container>
 
-            <View style={styles.button}>
-              <Button
-                mode="contained"
-                icon="person"
-                color='#90CAF9'
-                onPress={() => this.props.navigation.navigate('LogInTypeUser')}>
-                Iniciar Sesión</Button>
-            </View>
-
-        </View>
-      </View>
+      //
+      // <View >
+      //
+      //   <View style={styles.textContainer}>
+      //     <Text style={{fontSize:52, textAlign: 'center', textAlignVertical: 'center'}}>
+      //       Yo Soy Muy Inteligente UN</Text>
+      //   </View>
+      //
+      //   <View style={styles.buttonsContainer}>
+      //
+      //       <View style={styles.button}>
+      //
+      //
+      //         <Button
+      //           mode="contained"
+      //           icon="person-add"
+      //           onPress={() => this.props.navigation.navigate('MainSignUp')}>
+      //           Registro</Button>
+      //       </View>
+      //
+      //
+      //       <View style={styles.button}>
+      //         <Button
+      //           mode="contained"
+      //           icon="person"
+      //           color='#90CAF9'
+      //           onPress={() => this.props.navigation.navigate('LogInTypeUser')}>
+      //           Iniciar Sesión</Button>
+      //       </View>
+      //
+      //   </View>
+      // </View>
     );
   }
 }

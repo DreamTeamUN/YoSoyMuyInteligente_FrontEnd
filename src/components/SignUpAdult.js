ec7af69fcd0ca49a2c577b714800312021d7c53b
Juan Sebastian Peña <jspenaq@unal.edu.co>
2018-10-20 13:30:26 -0500

    Se implemento la el SignUpAdult

--- src/components/SignUpAdult.js
+++ src/components/SignUpAdult.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Alert, View } from 'react-native';
 import { Text, Button, Icon, Label, Form, Item, Input } from 'native-base';
 import axios from 'axios';
-import API_SIGN_UP_ADULT from '../config/const';
+import { API_SIGN_UP_ADULT } from '../config/const';
 import styles from '../styles';
 
 export default class SignUpAdult extends Component {
@@ -22,8 +22,8 @@ export default class SignUpAdult extends Component {
   }
 
 
-  //Validación Formulario
-  validate(username, password, password2, email) {
+  // Validación Formulario
+  _validate(username, password, password2, email) {
     formatotexto = /^[a-zA-Z0-9]+$/;
     formatomail = /\S+@\S+\.\S+/;
 
@@ -32,23 +32,39 @@ export default class SignUpAdult extends Component {
       && password == password2
       && password.length >= 6
     ) {
-      Alert.alert('El formulario fue llenado correctamente.');
+      // Alert.alert('El formulario fue llenado correctamente.');
+      return true;
     } else {
       Alert.alert('Hay errores en el formulario.');
+      return false;
     }
   }
 
+  // prototipo para despues validar cada paramentro individualmente:
+  // _validateUsername(text) {
+  //   let regExp = /^[a-zA-Z0-9]+$/;
+  //   if (!regExp.test(text)) {
+  //     this.setState({username:text})
+  //     return false
+  //   } else {
+  //     this.setState({username:text})
+  //     return true
+  //   }
+  // }
+
   _createAdult() {
-    Alert.alert("Creando adulto")
-    axios.post(API_SIGN_UP_ADULT, {
-      "usuario": {
-        "user": this.state.username,
-        "password": this.state.password,
-        "nombre": this.state.username,
-        "email": this.state.email,
-        "archivo_id": 0
-      }
-    });
+    if (this._validate(this.state.username, this.state.password, this.state.password2, this.state.email)) {
+      Alert.alert("Creando adulto")
+      axios.post(API_SIGN_UP_ADULT, {
+        "usuario": {
+          "user": this.state.username,
+          "password": this.state.password,
+          "nombre": this.state.username,
+          "email": this.state.email,
+          "archivo_id": 0
+        }
+      });
+    }
   };
 
   render() {
@@ -61,29 +77,35 @@ export default class SignUpAdult extends Component {
             <Label>Nombre de usuario</Label>
             <Input
               maxLength={45}
+              // onChangeText={(username) => this._validateUsername(username)}
               onChangeText={(username) => this.setState({ username })}
+              value={this.state.username}
             />
           </Item>
 
           <Item floatingLabel style={styles.adult_TextInput}>
             <Label>Contraseña</Label>
             <Input
-              onChangeText={
-                (password) => this.setState({ password })}
-              secureTextEntry={true} />
+              secureTextEntry={true}
+              onChangeText={(password) => this.setState({ password })}
+              value={this.state.password}
+            />
           </Item>
 
           <Item floatingLabel style={styles.adult_TextInput}>
             <Label>Repita su contraseña</Label>
             <Input
+              secureTextEntry={true}
               onChangeText={(password2) => this.setState({ password2 })}
-              secureTextEntry={true} />
+              value={this.state.password2}
+            />
           </Item>
-          
+
           <Item floatingLabel last style={styles.adult_TextInput}>
             <Label>Correo electrónico</Label>
             <Input
               onChangeText={(email) => this.setState({ email })}
+              alue={this.state.email}
             />
           </Item>
 
@@ -96,17 +118,17 @@ export default class SignUpAdult extends Component {
               <Icon name="person-add" />
               <Text>Añadir Estudiante</Text>
             </Button>
-          {/* </View> */}
+            {/* </View> */}
 
-          {/* <View style={styles.button}> */}
+            {/* <View style={styles.button}> */}
             <Button iconLeft rounded
               style={styles.buttondark}
-              onPress={() => this.validate(this.state.username, this.state.password, this.state.password2, this.state.email)}>
+              onPress={this._createAdult.bind(this)} >
               <Icon type="MaterialIcons" name="done" />
               <Text>Finalizar Registro</Text>
             </Button>
           </View>
-          
+
         </View>
 
       </View>

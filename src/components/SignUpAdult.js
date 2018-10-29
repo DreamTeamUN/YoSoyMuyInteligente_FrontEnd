c5c8a44813d8f4d3fb1febf5a03ddb21de79b252
Juan Sebastian Peña <jspenaq@unal.edu.co>
2018-10-28 19:17:34 -0500

    Se notifica cuando se envia la peticion de crear un usuario al back y en consola se imprimen los errores cuando la peticion tiene status 422

--- src/components/SignUpAdult.js
+++ src/components/SignUpAdult.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, View, ScrollView } from 'react-native';
+import { ActivityIndicator, Alert, View, ScrollView } from 'react-native';
 import { Container, Text, Button, Icon, Label, Form, Item, Input } from 'native-base';
 import axios from 'axios';
 import { API_SIGN_UP_ADULT } from '../config/const';
@@ -18,7 +18,8 @@ export default class SignUpAdult extends Component {
       password2: '',
       email: '',
       birthdate: '',
-      errors: [],
+      // errors: [],
+      isLoading: false,
     };
   }
 
@@ -41,9 +42,9 @@ export default class SignUpAdult extends Component {
     }
 
     try {
-      
+
     } catch (error) {
-      
+
     }
   }
 
@@ -61,7 +62,8 @@ export default class SignUpAdult extends Component {
 
   async _createAdult() {
     if (this._validate(this.state.username, this.state.password, this.state.password2, this.state.email)) {
-      Alert.alert("Creando adulto")
+      this.setState({ isLoading: true })
+      // Alert.alert("Creando adulto")
       try {
         // Axios version:
         // axios.post(API_SIGN_UP_ADULT, {
@@ -92,8 +94,24 @@ export default class SignUpAdult extends Component {
           })
         });
 
-        let res = await response.text();
-        console.log("res is: " + res);
+        // let res = await response.text();
+        // console.log("res is: " + res);
+        let status = response.status
+        console.log("res status: " + status);
+
+        if (status == 201) {
+          console.log("Nuevo usuario!");
+        } else if (status == 422) {
+
+          let res = await response.json();
+          if (res.hasOwnProperty("user")) {
+            console.log(res.user.toString())
+          } else if (res.hasOwnProperty("email")) {
+            console.log(res.email.toString())
+          }
+        }
+        this.setState({ isLoading: false })
+
       } catch (error) {
         console.log("catch errors: " + error)
       }
@@ -101,72 +119,84 @@ export default class SignUpAdult extends Component {
   };
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <View style={styles.home_TextContainer}>
+            <Text style={styles.headling}>Creando usuario</Text>
+            <ActivityIndicator size="large" color="#4267B2" />
+          </View>
+        </View>
+      );
+    }
     return (
       // <Container>
-        <View>
+      <View>
 
-          <Form style={styles.adult_TextInputContainer}>
+        <Form style={styles.adult_TextInputContainer}>
 
-            <Item floatingLabel style={styles.adult_TextInput}>
-              <Label style={{color: '#E40808',}}>Nombre de usuario</Label>
-              <Input success
-                maxLength={45}
-                // onChangeText={(username) => this._validateUsername(username)}
-                onChangeText={(username) => this.setState({ username })}
-                value={this.state.username}
-              />
-              <Icon name='checkmark-circle' />
-            </Item>
+          <Item floatingLabel style={styles.adult_TextInput}>
+            <Label>Nombre de usuario</Label>
+            {/* <Label style={{color: '#E40808',}}>Nombre de usuario</Label> */}
+            <Input
+              // <Input success
+              maxLength={45}
+              // onChangeText={(username) => this._validateUsername(username)}
+              onChangeText={(username) => this.setState({ username })}
+              value={this.state.username}
+            />
+            {/* <Icon name='checkmark-circle' /> */}
+          </Item>
 
-            <Item floatingLabel style={styles.adult_TextInput}>
-              <Label>Contraseña</Label>
-              <Input
-                secureTextEntry={true}
-                onChangeText={(password) => this.setState({ password })}
-                value={this.state.password}
-              />
-            </Item>
+          <Item floatingLabel style={styles.adult_TextInput}>
+            <Label>Contraseña</Label>
+            <Input
+              secureTextEntry={true}
+              onChangeText={(password) => this.setState({ password })}
+              value={this.state.password}
+            />
+          </Item>
 
-            <Item floatingLabel style={styles.adult_TextInput}>
-              <Label>Repita su contraseña</Label>
-              <Input
-                secureTextEntry={true}
-                onChangeText={(password2) => this.setState({ password2 })}
-                value={this.state.password2}
-              />
-            </Item>
+          <Item floatingLabel style={styles.adult_TextInput}>
+            <Label>Repita su contraseña</Label>
+            <Input
+              secureTextEntry={true}
+              onChangeText={(password2) => this.setState({ password2 })}
+              value={this.state.password2}
+            />
+          </Item>
 
-            <Item floatingLabel last style={styles.adult_TextInput}>
-              <Label>Correo electrónico</Label>
-              <Input
-                onChangeText={(email) => this.setState({ email })}
-                value={this.state.email}
-              />
-            </Item>
+          <Item floatingLabel last style={styles.adult_TextInput}>
+            <Label>Correo electrónico</Label>
+            <Input
+              onChangeText={(email) => this.setState({ email })}
+              value={this.state.email}
+            />
+          </Item>
 
-          </Form>
+        </Form>
 
-          <View style={styles.adult_ButtonsContainer}>
-            <View style={styles.button}>
-              <Button iconLeft rounded style={styles.buttonclear}
-                onPress={() => this.props.navigation.navigate('AddStudent')}>
-                <Icon name="person-add" />
-                <Text>Añadir Estudiante</Text>
-              </Button>
-              {/* </View> */}
+        <View style={styles.adult_ButtonsContainer}>
+          <View style={styles.button}>
+            <Button iconLeft rounded style={styles.buttonclear}
+              onPress={() => this.props.navigation.navigate('AddStudent')}>
+              <Icon name="person-add" />
+              <Text>Añadir Estudiante</Text>
+            </Button>
+            {/* </View> */}
 
-              {/* <View style={styles.button}> */}
-              <Button iconLeft rounded
-                style={styles.buttondark}
-                onPress={this._createAdult.bind(this)} >
-                <Icon type="MaterialIcons" name="done" />
-                <Text>Finalizar Registro</Text>
-              </Button>
-            </View>
-
+            {/* <View style={styles.button}> */}
+            <Button iconLeft rounded
+              style={styles.buttondark}
+              onPress={this._createAdult.bind(this)} >
+              <Icon type="MaterialIcons" name="done" />
+              <Text>Finalizar Registro</Text>
+            </Button>
           </View>
 
         </View>
+
+      </View>
       // </Container>
     );
   }
